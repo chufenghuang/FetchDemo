00aab10f2ac9da56273ad6b62cc1e3a14379ddc6
chufenghuang <bulahuang@icloud.com>
2024-03-08 09:15:04 -0600

    Initial Commit

---
+++ FetchDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FetchDemo
+//
+//  Created by 黄先生 on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FetchDemo/FetchDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDemoApp.swift
+//  FetchDemo
+//
+//  Created by 黄先生 on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
